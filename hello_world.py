747a2b53306d9a0fbdc8bc74763c9e394589b7d2
yumatov <yumatov.vladimir@220-volt.ru>
2018-10-25 11:30:22 +0300

    Добавлена возможность использования бибилиотеки cwltool

--- hello_world.py
+++ hello_world.py
@@ -2,15 +2,32 @@
 Принимает в качестве аргументов:
 1. CWL-скрипт
 2. аргументы (?) для CWL-скрипта
-
 Проверить: python hello_world.py 1st-tool.cwl echo-job.yml
+
+Так же можно запустить без аргументов, тогда вызовется функция use_import, которая использует библиотеку cwltool
 """
 
-import subprocess
 import sys
 
-if __name__ == '__main__':
-    script_path = sys.argv[1]
-    args_path = sys.argv[2]
+
+def use_subprocess(script_path, args_path):
+    from subprocess import call
     cwl_command = ['cwl-runner', script_path, args_path]
-    subprocess.call(cwl_command)
+    call(cwl_command)
+
+
+def use_import():
+    from cwltool.factory import Factory
+    import yaml
+    fac = Factory()
+
+    echo = fac.make("1st-tool.cwl")
+    with open('echo-job.yml', 'r') as f:
+        print(echo(**yaml.safe_load(f)))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        use_subprocess(sys.argv[1], sys.argv[2])
+    else:
+        use_import()
