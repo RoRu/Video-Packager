588e392737fff610dc512232e4096d08424039fe
yumatov <yumatov.vladimir@220-volt.ru>
2018-10-31 15:16:11 +0300

    Python runner now has command line arguments and description

--- pyrunner/readme.md
+++ pyrunner/readme.md
@@ -1,10 +1,26 @@
-##### Установка зависимостей: 
+## Primitive Python runner for CWL scripts
+
+#### Requirements:
+    
+*       Python 3.6+
+*       pip install -r requirements.txt
+
+
+#### Usage:
  
-    pip install -r requirements.txt
+    runner.py [-h] [-s] script_path args_path
 
 
-##### Запуск:
+###### For example:
+`python runner.py -s ../cwl-scripts/guesser/guesser.cwl ../cwl-scripts/guesser/guesser-job.yml`
 
-##### Проверка: 
 
-    python runner.py ../cwl-scripts/hello-world/1st-tool.cwl ../cwl-scripts/hello-world/echo-job.yml
+**Positional arguments**:
+
+    script_path       path to CWL scrpt
+    args_path         path to script arguments
+
+**Optional arguments**:
+
+    -h, --help        show this help message and exit
+    -s, --subprocess  run script in separate process
--- pyrunner/runner.py
+++ pyrunner/runner.py
@@ -1,28 +1,30 @@
-"""
-
-"""
-
-import sys
+import argparse
 
 
-def use_subprocess(script_path, args_path):
+def run_in_subprocess(script_path, args_path):
     from subprocess import call
     cwl_command = ['cwl-runner', script_path, args_path]
     call(cwl_command)
 
 
-def use_import():
+def run(script_path, args_path):
     from cwltool.factory import Factory
     import yaml
     fac = Factory()
 
-    echo = fac.make("1st-tool.cwl")
-    with open('echo-job.yml', 'r') as f:
+    echo = fac.make(script_path)
+    with open(args_path, 'r') as f:
         print(echo(**yaml.safe_load(f)))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        use_subprocess(sys.argv[1], sys.argv[2])
+    parser = argparse.ArgumentParser(description='Python runner for CWL scripts.')
+    parser.add_argument('-s', '--subprocess', action='store_true', help='run script in separate process')
+    parser.add_argument('script_path', type=str, help='path to CWL scrpt')
+    parser.add_argument('args_path', type=str, help='path to script arguments')
+    args = parser.parse_args()
+
+    if args.subprocess:
+        run_in_subprocess(args.script_path, args.args_path)
     else:
-        use_import()
+        run(args.script_path, args.args_path)
